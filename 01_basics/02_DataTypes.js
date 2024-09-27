af6666228250ae2edb0b7f1f7609b09304b964dd
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2024-09-27 13:18:04 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert ("jay")
+
+let name = "jay"
+let age = 19
+let collage;
+let tempreter=null;
+
+/*
+string => ""
+boolean => ture/false
+number
+null => standalone
+undefined
+symbol => unique
+object
+*/
+
+console.log(typeof null)
