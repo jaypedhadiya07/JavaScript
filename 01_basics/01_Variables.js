f1a7e7ef6a15a4c0edfce0f76a8d4cb56074f694
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2024-09-27 13:12:56 +0000

    Variables.js

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 1111
+let userName = "jaypatel"
+var accountPassword = "2222"
+let accountState;
+
+city = "rajkot"
+
+city = "jamnagar"
+
+console.table([accountId , userName , accountPassword , city , accountState]);
