34e89b2f74bd5a739f4749a534f8ace33f2f3764
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2024-09-29 05:16:27 +0000

    Math basics

--- 01_basics/07_Num_and_Math.js
+++ 01_basics/07_Num_and_Math.js
@@ -13,3 +13,24 @@ console.log(balance.toPrecision(3));
 
 let hundreds = 10000000000
 console.log(hundreds.toLocaleString('en-IN')); //Indian Value 
+
+// ++++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4)); // only - to + not + to -
+console.log(Math.round(45.83));
+console.log(Math.ceil(45.19));
+console.log(Math.floor(45.89));
+console.log(Math.min(2 , 7 , 9 , 10 , 78));
+console.log(Math.max(2 , 7 , 9 , 10 , 78));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+let min = 10 
+let max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
