a7742586ebc4b431f7c0af7de31b3296d7975d23
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2025-02-01 12:20:06 +0000

    while and do while loooop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let i = 0
+while (i <= 20) {
+    console.log(i);
+    i = i + 2
+}
+
+let arr1 = ["Hanuman", "ram", "syam", "laxman", "kush", "love"]
+let a = 0
+while (a < arr1.length) {
+    console.log("Hero is ", arr1[a]);
+    a = a + 1
+}
+
+// do while looop
+let b = 11
+do {
+    console.log(b);
+    b++
+} while (b <= 20)
